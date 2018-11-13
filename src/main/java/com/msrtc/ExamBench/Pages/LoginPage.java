3ff130989e07f2455da1ecfd76470c50a1104e16
Rampal Singh <rampal.a.singh@gmail.com>
2018-11-13 15:16:01 +0530

    Deo

---
+++ src/main/java/com/msrtc/ExamBench/Pages/LoginPage.java
@@ -0,0 +1,18 @@
+package com.msrtc.ExamBench.Pages;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com.msrtc.ExamBench.Base.BasePage;
+
+public class LoginPage extends BasePage {
+
+	public LoginPage() throws Exception {
+		PageFactory.initElements(driver, this);
+	}
+
+	public HomePage doLogin() {
+		
+
+	}
+
+}
